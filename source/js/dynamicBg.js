a605944175290f5ba9ddb7bb5dbb7c22bcc7e05f
zhangyuhan <1355166049@qq.com>
2024-06-06 13:56:31 +0800

    blog update

--- source/js/dynamicBg.js
+++ source/js/dynamicBg.js
@@ -1,8 +1,14 @@
 function dynamicBg() {
+  let img = new Image()
   setInterval(() => {
-    document.getElementById('web_bg').style.backgroundImage =
-      'url(https://www.loliapi.com/acg/?number=' + Math.random() + ')'
+    let url = 'https://www.loliapi.com/acg/?number=' + Math.random()
+    img.src = url
+    img.addEventListener('load', function () {
+      $('#web_bg').fadeOut(3000, function () {
+        document.getElementById('web_bg').style.backgroundImage = `url(${url})`
+        $('#web_bg').fadeIn(3000)
+      })
+    })
   }, 60000)
 }
-
 dynamicBg()
