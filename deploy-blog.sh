fd30593f48ecde93011077843559df0084a5baeb
zhangyuhan <1355166049@qq.com>
2024-06-19 14:52:16 +0800

    不每次都本地部署了

--- deploy-blog.sh
+++ deploy-blog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo `npx hexo cl`;
 echo `npx hexo g -d`;
-kill -9 $(lsof -i:4000 |awk '{print $2}' | tail -n 2);
-echo `nohup npx hexo s > log.log 2>&1 &`;
+#kill -9 $(lsof -i:4000 |awk '{print $2}' | tail -n 2);
+#echo `nohup npx hexo s > log.log 2>&1 &`;
